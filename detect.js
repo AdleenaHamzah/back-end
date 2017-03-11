6014137ce0ec6e0b74685cabf782d80869b2b875
AdleenaHamzah <adleenah@gmail.com>
2017-03-11 23:45:27 +0800

    init commit

---
+++ detect.js
@@ -0,0 +1,19 @@
+var openalpr = require ("node-openalpr");
+ 
+function identify (id, path) {
+    console.log (openalpr.IdentifyLicense (path, function (error, output) {
+        var results = output.results;
+        console.log (id +" "+ output.processing_time_ms +" "+ ((results.length > 0) ? results[0].plate : "No results"));
+    
+        if (id == 349) {
+            console.log (openalpr.Stop ());
+        }
+    }));
+}
+ 
+openalpr.Start ();
+openalpr.GetVersion ();
+ 
+for (var i = 0; i < 350; i++) {
+    identify (i, "lp.jpg");
+}
